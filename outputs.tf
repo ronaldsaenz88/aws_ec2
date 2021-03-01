264ce85fce067b9b01ea8047f20a388e242d1a6f
RSAENZ <ronaldsaenz88@gmail.com>
2021-02-28 23:12:52 -0500

    AWS - EC2 INSTANCE - #2

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,14 @@
 output "instance_id" {
+  description = "Id of instance created"
   value = "${aws_instance.web.id}"
 }
 
 output "instance_private_ip" {
+  description = "Private IP configured on instance"
   value = "${aws_instance.web.private_ip}"
+}
+
+output "tags" {
+  description = "List of tags of instances"
+  value       = aws_instance.this.*.tags
 }
